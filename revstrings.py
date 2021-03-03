bc5a70946859d201c97418125f5d5840018dce11
Kennis Brown <need4speed800@gmail.com>
2021-03-02 16:57:00 -0800

    Python Reverse Strings Script

---
+++ revstrings.py
@@ -0,0 +1,10 @@
+def reverse_slicing(s):
+   return s[::-1]
+input_str =r"""ability\ able about above accept 
+according account across act action activity actually add address administration admit adult affect after again against age agency agent ago"""
+
+final = reverse_slicing(input_str)
+
+if __name__ == "__main__":
+    print(final)
+
